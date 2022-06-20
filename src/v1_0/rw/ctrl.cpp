e1d075a77c88fa8a57aafa83050d66674d75132c
Krzysztof Frejlich <59435607+krzyfre@users.noreply.github.com>
2022-06-20 12:19:56 +0200

    GRASP-12267 Update src/v1_0/rw/ctrl.cpp
    as per @mkatliar comment
    
    Co-authored-by: Mikhail Katliar <cepstr@gmail.com>

--- src/v1_0/rw/ctrl.cpp
+++ src/v1_0/rw/ctrl.cpp
@@ -10,6 +10,6 @@ namespace abb :: rws :: v1_0 :: rw :: ctrl
         std::stringstream content;
         content << "restart-mode=" << restartMode;
 
-        client.httpPost(uri.str(), content.str(), std::set<Poco::Net::HTTPResponse::HTTPStatus> {Poco::Net::HTTPResponse::HTTP_NO_CONTENT, Poco::Net::HTTPResponse::HTTP_OK});
+        client.httpPost(uri.str(), content.str(), {Poco::Net::HTTPResponse::HTTP_NO_CONTENT, Poco::Net::HTTPResponse::HTTP_OK});
     }
 }
