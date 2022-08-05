79d94fb9f24d2d7e6a0295ec16b8a3287bfb97da
Michał Kowalik <michalk@nomagic.ai>
2022-08-05 13:56:24 +0200

    Extract logic for content logging.

--- src/rws_poco_client.cpp
+++ src/rws_poco_client.cpp
@@ -100,14 +100,33 @@ POCOResult POCOClient::httpDelete(const std::string& uri)
 }
 
 
-void logTrimmedContent(std::string const& content, int const logLimit)
+class ContentLogging
 {
-  int contentLength = content.length();
-  BOOST_LOG_TRIVIAL(debug)
-      << "Content: '" << content.substr(0, std::min(logLimit / 2, contentLength)) << " (...) "
-      << content.substr(std::max(0, contentLength - logLimit / 2), contentLength) << "'."
-      << "\nSome of the content omitted because it was " << contentLength << " characters long.";
-}
+public:
+
+    static void logFull(std::string const& content)
+    {
+      BOOST_LOG_TRIVIAL(debug) << "Content: '" << content << "'.";
+    }
+
+    static void logTrimmed(std::string const& content, int const logLimit)
+    {
+      int contentLength = content.length();
+      BOOST_LOG_TRIVIAL(debug)
+          << "Content: '" << content.substr(0, std::min(logLimit / 2, contentLength)) << "\n(...)\n"
+          << content.substr(std::max(0, contentLength - logLimit / 2), contentLength) << "'."
+          << "\nSome of the content omitted because it was " << contentLength << " characters long.";
+    }
+
+    static void logContent(std::string const& content)
+    {
+        if (content.length() < logLimit_)
+            ContentLogging::logFull(response_content);
+        else
+            ContentLogging::logTrimmed(response_content, 250);
+    }
+};
+
 
 POCOResult POCOClient::makeHTTPRequest(const std::string& method,
                                                    const std::string& uri,
@@ -135,22 +154,12 @@ POCOResult POCOClient::makeHTTPRequest(const std::string& method,
   try
   {
     BOOST_LOG_TRIVIAL(debug) << "Trying to " << method << " from uri=" << uri;
-
-    static int const logLimit = 250;
-    if (content.length() < logLimit)
-      BOOST_LOG_TRIVIAL(debug) << "Request content: '" << content << "'.";
-    else
-      logTrimmedContent(content, logLimit);
+    ContentLogging::logContent(content);
 
     sendAndReceive(request, response, content, response_content);
 
-    BOOST_LOG_TRIVIAL(debug) << "Got status=" << response.getStatus() << " when " << method
-                             << "ing from uri=" << uri;
-
-    if (response_content.length() < logLimit)
-      BOOST_LOG_TRIVIAL(debug) << "Response content: '" << response_content << "'.";
-    else
-      logTrimmedContent(response_content, logLimit);
+    BOOST_LOG_TRIVIAL(debug) << "Got status=" << response.getStatus() << " when " << method << "ing from uri=" << uri;
+    ContentLogging::logContent(response_content);
 
     // Check if the server has sent an update for the cookies.
     std::vector<HTTPCookie> temp_cookies;
