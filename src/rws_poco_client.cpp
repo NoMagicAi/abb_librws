a8bfa099261a9c803d0a5e7cf054f2c94f0c31a8
Krzysztof Frejlich <kf@nomagic.ai>
2025-09-09 12:28:27 +0200

    [All-Projects] ATASK-0 Longer timeout

--- src/rws_poco_client.cpp
+++ src/rws_poco_client.cpp
@@ -234,6 +234,7 @@ void POCOClient::sendAndReceive(HTTPRequest& request,
 
   try
   {
+    http_client_session_.setReceiveTimeout(Poco::Timespan(30, 0)); // 30 seconds timeout
     std::istream& response_content_stream = http_client_session_.receiveResponse(response);
 
     response_content.clear();
